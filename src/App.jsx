ba2d25229169b76211bb12d9e49eb64ef36c771b
Bruno Tenório <tenoriobn@gmail.com>
2023-09-04 15:48:43 -0300

    Aplicando estilos globais

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import EstilosGlobais from "./componentes/EstilosGlobais";
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -10,7 +11,7 @@ function App() {
 
   return (
     <FundoGradiente>
-
+      <EstilosGlobais />
     </FundoGradiente>
   )
 }

5a61db12674fd7f0e56f738616aa83859381a472
Bruno Tenório <tenoriobn@gmail.com>
2023-09-09 14:13:02 -0300

    Adicionando mais itens de navegação

--- src/componentes/BarraLateral/index.jsx
+++ src/componentes/BarraLateral/index.jsx
@@ -27,6 +27,24 @@ const BarraLateral = () => {
                     >
                         Mais vistas
                     </ItemNavegacao>
+                    <ItemNavegacao
+                        iconeAtivo="/icones/mais-curtidas-ativo.png"
+                        iconeInativo="/icones/mais-curtidas-inativo.png"
+                    >
+                        Mais curtidas
+                    </ItemNavegacao>
+                    <ItemNavegacao
+                        iconeAtivo="/icones/novas-ativo.png"
+                        iconeInativo="/icones/novas-inativo.png"
+                    >
+                        Novas
+                    </ItemNavegacao>
+                    <ItemNavegacao
+                        iconeAtivo="/icones/surpreenda-me-ativo.png"
+                        iconeInativo="/icones/surpreenda-me-inativo.png"
+                    >
+                        Surpreenda-me
+                    </ItemNavegacao>
                 </ListaEstilizada>
             </nav>
         </aside>

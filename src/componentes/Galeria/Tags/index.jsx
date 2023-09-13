b60f4f01a5adfc08ca5811a546a69789d9d3a931
Bruno Tenório <tenoriobn@gmail.com>
2023-09-13 09:50:33 -0300

    Lidando com as tags

--- src/componentes/Galeria/Tags/index.jsx
+++ src/componentes/Galeria/Tags/index.jsx
@@ -1,6 +1,11 @@
+import tags from './tags.json';
+
 const Tags = () => {
     return (
-        <p>Tags</p>
+        <>
+            <p>Busque por tags:</p>
+            {tags.map(tag => <button key={tag.id}>{tag.titulo}</button>)}
+        </>
     )
 }
 

4c11a68207d05ba76475a303b61de77e21cdac76
Bruno Tenório <tenoriobn@gmail.com>
2023-09-13 09:55:29 -0300

    Adicionando estilos a Tag

--- src/componentes/Galeria/Tags/index.jsx
+++ src/componentes/Galeria/Tags/index.jsx
@@ -1,10 +1,33 @@
+import { styled } from 'styled-components';
 import tags from './tags.json';
+
+const TagTitulo = styled.h3`
+    color: #D9D9D9;
+    font-size: 24px;
+    margin: 0;
+`;
+
+const Tag = styled.button`
+    font-size: 24px;
+    color: #FFFFFF;
+    background: rgba(217, 217, 217, 0.3);
+    border-radius: 10px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    padding: 12px;
+    box-sizing: border-box;
+    border: 2px solid transparent;
+
+    &:hover {
+        border-color: #C98CF1;
+    }
+`
 
 const Tags = () => {
     return (
         <>
-            <p>Busque por tags:</p>
-            {tags.map(tag => <button key={tag.id}>{tag.titulo}</button>)}
+            <TagTitulo>Busque por tags:</TagTitulo>
+            {tags.map(tag => <Tag key={tag.id}>{tag.titulo}</Tag>)}
         </>
     )
 }

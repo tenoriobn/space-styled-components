6bfa811b862062c6f059dc71731b50112691296a
Bruno Tenório <tenoriobn@gmail.com>
2023-09-14 17:58:31 -0300

    Fundo com opacidade

--- src/componentes/ModalZoom/index.jsx
+++ src/componentes/ModalZoom/index.jsx
@@ -1,12 +1,34 @@
+import styled from "styled-components";
+import Imagem from "../Galeria/Imagem";
+
+const OverLay = styled.div`
+    background-color: rgba(0, 0, 0, 0.7);
+
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+`
+
+const DialogEstilizado = styled.dialog`
+    position: absolute;
+    top: 294px;
+`
+
 const ModalZoom = ( { foto } ) => {
     return (
         <>        
-            <dialog open={!!foto}>
-                <p>Exemplo do Mozilla</p>
-                <form method="dialog">
-                    <button type="submit">ok</button>
-                </form>
-            </dialog>
+            {foto && <>
+            <OverLay />
+                <DialogEstilizado open={!!foto}>
+                    <Imagem foto={foto} expandida={true} />
+                    <p>Exemplo do Mozilla</p>
+                    <form method="dialog">
+                        <button type="submit">ok</button>
+                    </form>
+                </DialogEstilizado>
+            </>}
         </>
     )
 }
